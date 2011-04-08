a98af20105d34387ee4ada4b9730b5d3cfd58401
Patrick Morgan <p.w.morgan@gmail.com>
2011-04-07 21:39:44 -0700

    Added time based speed to players.

--- plumbergone.py
+++ plumbergone.py
@@ -7,6 +7,10 @@ width = 1024
 height = 512
 size = width, height
 white = 255, 255, 255
+clock = pygame.time.Clock()
+mainloop = True
+FPS = 60
+playtime = 0
 
 #Image files
 player_image = "media/player.bmp"
@@ -15,24 +19,24 @@ pipe_image = "media/pipe.bmp"
 screen = pygame.display.set_mode(size)
 
 class player():
-                     
 	def __init__(self, x, y, image):
 		self.image = pygame.image.load(image)
 		self.rect = self.image.get_rect()
 		self.rect.centerx = x
 		self.rect.centery = y
-		self.start = [x, y]
+		self.x = x
+		self.y = y
 		self.velocity = [0, 0]
-		self.speed = 90 #pixels a second
-		self.movement = 0
+		self.speed = 80 #pixels a second
 		
 	def reset(self):
 		self.velocity = [0, 0]
-		self.rect.centerx = self.start[0]
-		self.rect.centery = self.start[1]
+		self.rect.centerx = self.x
+		self.rect.centery = self.y
 
-	def movement(self):
-		pass
+	def movement(self, direction):
+		self.velocity[0] = self.speed * direction[0]
+		self.velocity[1] = self.speed * direction[1]
 
 #Establish players and starting positions
 player1 = player(35, 35, player_image)
@@ -55,35 +59,46 @@ def collision_check(player, width, height):
 			return False
 	return True
 
-while True:
+while mainloop:
+    #Calculate time.
+	milliseconds = clock.tick(FPS)
+	seconds = milliseconds / 1000.0
+	playtime += seconds
+	
+	#Watch for key events.
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			sys.exit()
 		if event.type == pygame.KEYDOWN:
 			keystate = pygame.key.get_pressed()
 			if keystate[player1.up]:
-				player1.velocity = [0, -1]
+				player1.movement([0, -1])
 			elif keystate[player1.down]:
-				player1.velocity = [0, 1]
+				player1.movement([0, 1])
 			elif keystate[player1.right]:
-				player1.velocity = [1, 0]
+				player1.movement([1, 0])
 			elif keystate[player1.left]:
-				player1.velocity = [-1, 0]
+				player1.movement([-1, 0])
 
 			if keystate[player2.up]:
-				player2.velocity = [0, -1]
+				player2.movement([0, -1])
 			elif keystate[player2.down]:
-				player2.velocity = [0, 1]
+				player2.movement([0, 1])
 			elif keystate[player2.right]:
-				player2.velocity = [1, 0]
+				player2.movement([1, 0])
 			elif keystate[player2.left]:
-				player2.velocity = [-1, 0]
+				player2.movement([-1, 0])
 
+	pygame.display.set_caption("[FPS]: %.2f" % clock.get_fps())
+	screen.fill(white)
 	for players in [player1, player2]:
 		if not collision_check(players, width, height):
-			players.rect = players.rect.move(players.velocity)
-			
-	screen.fill(white)
-	screen.blit(player1.image, player1.rect)
-	screen.blit(player2.image, player2.rect)
+			#players.movement(seconds)
+			players.x += players.velocity[0] * seconds
+			players.y += players.velocity[1] * seconds
+			players.rect.centerx = players.x
+			players.rect.centery = players.y
+		#if I include the below line in the if statment, I can cut out the player art when they crash
+		screen.blit(players.image, players.rect)
+	pygame.display.set_caption("[FPS]: %.2f." % clock.get_fps())
 	pygame.display.flip()  
