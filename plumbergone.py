b2168bbf13a949b207fe93b8554c8962c044a0f2
Patrick Morgan <p.w.morgan@gmail.com>
2011-04-09 20:08:10 -0700

    Added gridmap logic and better collision detection.

--- plumbergone.py
+++ plumbergone.py
@@ -12,6 +12,61 @@ mainloop = True
 FPS = 60
 playtime = 0
 
+#Grid Settings
+cell_size = 35 #pixels
+borderx = (width % cell_size) / 2 + cell_size
+bordery = (height % cell_size) / 2 + cell_size
+cellsx = width / cell_size
+cellsy = height / cell_size
+cell_count_x = (width - (2 * borderx)) / cell_size
+cell_count_y = (height - (2 * bordery)) / cell_size
+	
+class gameboard():
+	def __init__(self, x, y):
+		self.grid = []
+		self.new(x, y)
+		self.previous = []
+		self.lowest = []
+
+	def new(self, x, y):
+		for row in range(x):
+			self.grid.append([])
+			for column in range(y):
+				self.grid[row].append(0)
+
+	def add_pipe(self, player, x, y):
+		row = (x - borderx) / cell_size
+		column = (y - bordery) / cell_size
+		self.grid[row][column] = player.number
+				
+	def collision(self, x, y):
+		row = (x - borderx) / cell_size
+		column = (y - bordery) / cell_size
+		if row < 0:
+			return True
+		elif column < 0:
+			return True
+		elif row >= len(self.grid):
+			return True
+		elif column >= len(self.grid[row]):
+			return True
+		if self.grid[row][column] != 0:
+			return True
+		else:
+		   	return False
+                       
+board = gameboard(cell_count_x, cell_count_y)
+
+def collision_check(player, width, height):
+	if 0 < player.rect.centerx < width:
+		if 0 < player.rect.centery < height:
+			return False
+	return True        
+
+	def store(self):
+		gameboard.lowest = gameboard.previous[0:]
+		gameboard.previous = gameboard.grid[0:]
+
 #Image files
 player_image = "media/player.bmp"
 pipe_image = "media/pipe.bmp"
@@ -19,7 +74,8 @@ pipe_image = "media/pipe.bmp"
 screen = pygame.display.set_mode(size)
 
 class player():
-	def __init__(self, x, y, image):
+	def __init__(self, number, x, y, image):
+		self.number = number
 		self.image = pygame.image.load(image)
 		self.rect = self.image.get_rect()
 		self.rect.centerx = x
@@ -27,7 +83,7 @@ class player():
 		self.x = x
 		self.y = y
 		self.velocity = [0, 0]
-		self.speed = 80 #pixels a second
+		self.speed = 85 #pixels a second
 		
 	def reset(self):
 		self.velocity = [0, 0]
@@ -38,9 +94,17 @@ class player():
 		self.velocity[0] = self.speed * direction[0]
 		self.velocity[1] = self.speed * direction[1]
 
+class pipes():
+	def __init__(self):
+		pass
+
 #Establish players and starting positions
-player1 = player(35, 35, player_image)
-player2 = player(990, 475, player_image)
+startx1 = borderx + (cell_size / 2)
+starty1 = bordery + (cell_size / 2)
+startx2 = width - startx1
+starty2 = height - starty1
+player1 = player(1, startx1, starty1, player_image)
+player2 = player(2, startx2, starty2, player_image)
 
 #Control Scheme
 player1.up = K_UP
@@ -52,12 +116,6 @@ player2.up = K_w
 player2.down = K_s
 player2.left = K_a
 player2.right = K_d
-
-def collision_check(player, width, height):
-	if 0 < player.rect.centerx < width:
-		if 0 < player.rect.centery < height:
-			return False
-	return True
 
 while mainloop:
     #Calculate time.
@@ -71,34 +129,27 @@ while mainloop:
 			sys.exit()
 		if event.type == pygame.KEYDOWN:
 			keystate = pygame.key.get_pressed()
-			if keystate[player1.up]:
-				player1.movement([0, -1])
-			elif keystate[player1.down]:
-				player1.movement([0, 1])
-			elif keystate[player1.right]:
-				player1.movement([1, 0])
-			elif keystate[player1.left]:
-				player1.movement([-1, 0])
+			for players in [player1, player2]:
+				if keystate[players.up]:
+					players.movement([0, -1])
+				elif keystate[players.down]:
+					players.movement([0, 1])
+				elif keystate[players.right]:
+					players.movement([1, 0])
+				elif keystate[players.left]:
+					players.movement([-1, 0])
 
-			if keystate[player2.up]:
-				player2.movement([0, -1])
-			elif keystate[player2.down]:
-				player2.movement([0, 1])
-			elif keystate[player2.right]:
-				player2.movement([1, 0])
-			elif keystate[player2.left]:
-				player2.movement([-1, 0])
-
+	#Display FPS
 	pygame.display.set_caption("[FPS]: %.2f" % clock.get_fps())
 	screen.fill(white)
+
+	#Calculate player movement
 	for players in [player1, player2]:
-		if not collision_check(players, width, height):
-			#players.movement(seconds)
+		if not board.collision(int(players.x), int(players.y)):
 			players.x += players.velocity[0] * seconds
 			players.y += players.velocity[1] * seconds
 			players.rect.centerx = players.x
 			players.rect.centery = players.y
-		#if I include the below line in the if statment, I can cut out the player art when they crash
 		screen.blit(players.image, players.rect)
-	pygame.display.set_caption("[FPS]: %.2f." % clock.get_fps())
+
 	pygame.display.flip()  
